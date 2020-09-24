4aad14782e091d6f6c6aedd60c7c5131968ac314
Ömer Keskin <omer.keskin@softtech.com.tr>
2020-09-24 23:14:24 +0300

    Sqlite used

--- app/src/main/java/com/omerkeskin/artbook/ArtBookActivity.java
+++ app/src/main/java/com/omerkeskin/artbook/ArtBookActivity.java
@@ -1,24 +1,134 @@
 package com.omerkeskin.artbook;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class ArtBookActivity extends AppCompatActivity {
+
+    Bitmap selectedImage;
+    ImageView ivArtBook;
+    EditText etArtName, etPainterName, etYear;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_art_book);
+        ivArtBook = findViewById(R.id.ivArtBook);
+        etArtName = findViewById(R.id.etArtName);
+        etPainterName = findViewById(R.id.etPainterName);
+        etYear = findViewById(R.id.etYear);
+        btnSave = findViewById(R.id.btnSave);
     }
 
     public void onIvArtBookClicked(View view){
-        System.out.println("Here");
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }else{
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intentToGallery, 2);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intentToGallery, 2);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == 2 && RESULT_OK == resultCode && data != null){
+            Uri imageData = data.getData();
+            try {
+                if(Build.VERSION.SDK_INT >= 28){
+                    ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), imageData);
+                    selectedImage = ImageDecoder.decodeBitmap(source);
+                    ivArtBook.setImageBitmap(selectedImage);
+                }else{
+                    selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageData);
+                    ivArtBook.setImageBitmap(selectedImage);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void onBtnSaveClicked(View view){
-        System.out.println("Here btn");
+        String artName = etArtName.getText().toString();
+        String painterName = etPainterName.getText().toString();
+        String year = etYear.getText().toString();
+        Bitmap smallImage = makeSmallerImage(selectedImage, 300);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        smallImage.compress(Bitmap.CompressFormat.PNG, 50, outputStream);
+        byte[] byteArray = outputStream.toByteArray();
+
+        try{
+            SQLiteDatabase database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts  (id  INTEGER PRIMARY KEY , artname VARCHAR, paintername VARCHAR, year VARCHAR, image BLOB)");
+
+            String sqlString = "INSERT INTO arts (artname, paintername, year, image) values (?, ? , ? , ?)";
+            SQLiteStatement sqLiteStatement = database.compileStatement(sqlString);
+            sqLiteStatement.bindString(1, artName);
+            sqLiteStatement.bindString(2, painterName);
+            sqLiteStatement.bindString(3, year);
+            sqLiteStatement.bindBlob(4, byteArray);
+            sqLiteStatement.execute();
+
+        }catch (Exception ex){
+
+        }
+        finish();
+
+    }
+
+    public Bitmap makeSmallerImage(Bitmap image, int maximumSize){
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float)height;
+        if(bitmapRatio > 1){
+            width = maximumSize;
+            height = (int) (width / bitmapRatio);
+        }else{
+            height = maximumSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true);
     }
 
 
--- app/src/main/java/com/omerkeskin/artbook/MainActivity.java
+++ app/src/main/java/com/omerkeskin/artbook/MainActivity.java
@@ -4,17 +4,55 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvArt;
+    ArrayList<String> nameArray ;
+    ArrayList<Integer> idArray ;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvArt = findViewById(R.id.lvArt);
+        nameArray = new ArrayList<>();
+        idArray = new ArrayList<>();
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nameArray);
+        lvArt.setAdapter(arrayAdapter);
+
+        getData();
+    }
+
+    public void getData(){
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+            Cursor cursor = database.rawQuery("SELECT * FROM arts", null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while(cursor.moveToNext()){
+                nameArray.add(cursor.getString(nameIx));
+                idArray.add(cursor.getInt(idIx));
+            }
+            arrayAdapter.notifyDataSetChanged();
+            cursor.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 
     @Override
